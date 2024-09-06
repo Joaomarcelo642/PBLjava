4f385f4048bd46c1999c2c7a0efcf1d7106500bf
João Marcelo Nascimento Fernandes <125520966+Joaomarcelo642@users.noreply.github.com>
2024-09-05 21:29:16 -0300

    Quase pronto

--- vendaingressos/src/vendaingressos/Ingresso.java
+++ vendaingressos/src/vendaingressos/Ingresso.java
@@ -1,15 +1,19 @@
 package vendaingressos;
 
+import java.util.Objects;
+
 public class Ingresso {
     private Evento evento;
     private double preco;
-    private String assento
+    private String assento;
+    private Boolean isAtivo;
 
     //Construtor
     public Ingresso(Evento evento, double preco, String assento) {
         this.evento = evento;
         this.preco = preco;
         this.assento = assento;
+        this.isAtivo = true;
     }
 
     //Sets e Gets
@@ -34,6 +38,42 @@ public class Ingresso {
         this.assento = assento;
     }
 
+    public Boolean getIAtivo() {
+        return isAtivo;
+    }
+    public void setIAtivo(Boolean isAtivo) {
+        this.isAtivo = isAtivo;
+    }
+
     //Metodos para testes
-    public Boolean isAtivo(){}
+    public Boolean isAtivo(){
+        return isAtivo;
+    }
+
+    public Boolean cancelar(){
+        if(isAtivo && evento.isAtivo()){
+            isAtivo = false;
+            return true;
+        }else return false;
+    }
+
+    public Boolean reativar(){
+        if(!isAtivo && evento.isAtivo()){
+            isAtivo = true;
+            return true;
+        }else return false;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ingresso ingresso = (Ingresso) o;
+        return Double.compare(preco, ingresso.preco) == 0 && Objects.equals(evento, ingresso.evento) && Objects.equals(assento, ingresso.assento);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(evento, preco, assento);
+    }
 }
